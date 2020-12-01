da36e76534db66e2052334dbe295ad7c93ec654b
kanisimov <kanisimov@xmlshop.com>
2020-12-01 15:38:47 +0200

    add file to do common git commands to all bundles

---
+++ cgit.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+declare -a dirs
+COM=$@
+
+pathToProject=~/public_html/m_site
+
+cd pathToProject
+
+eval $COM
+
+cd "${pathToProject}/martenasoft"
+
+i=1
+
+for f in *;
+do
+  if [ -d "$f" ]; then
+    dirs[i++]="$f"
+  fi
+done
+
+for dir in "${dirs[@]}"
+do
+  cd "${pathToProject}/martenasoft/${dir}"
+  pwd
+  eval $COM
+done
